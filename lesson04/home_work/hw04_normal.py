5a3208520b869bc09ca50530ee69b35a95b00ed0
oly <46057808+olystrel@users.noreply.github.com>
2019-01-24 07:48:39 +0100

    fourth_lesson_normal

--- lesson04/home_work/hw04_normal.py
+++ lesson04/home_work/hw04_normal.py
@@ -3,7 +3,7 @@
 # 1 или более символов в верхнем регистре.
 # Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
 # Решить задачу двумя способами: с помощью re и без.
-
+import re
 line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'\
        'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'\
@@ -20,6 +20,25 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
        'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'\
        'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
 
+line_str = re.findall(r"[a-z]+", line)
+print("с использованием модуля re: \n",line_str)
+
+#без re
+
+lst = []
+small_word= ""
+for i  in line:
+    if i.islower():
+        small_word += i
+    elif len(small_word) != 0:
+        lst.append(small_word)
+        small_word = ""
+
+if len(small_word) != 0:
+    lst.append(small_word)
+
+print("Без использования re", lst)
+
 
 # Задание-2:
 # Вывести символы в верхнем регистре, слева от которых находятся
@@ -28,6 +47,7 @@ line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'\
 # "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
 # нужно получить список строк: ['AY', 'NOGI', 'P']
 # Решить задачу двумя способами: с помощью re и без.
+import re
 
 line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'\
@@ -45,9 +65,48 @@ line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'\
        'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'\
        'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
 
+
+lst1 = re.findall(r"[a-z]{2}([A-Z]+)[A-Z]{2}", line_2)
+print(lst1)
+
+
+
+
+
 # Задание-3:
 # Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
 # произвольными целыми цифрами, в результате в файле должно быть
 # 2500-значное произвольное число.
 # Найдите и выведите самую длинную последовательность одинаковых цифр
 # в вышезаполненном файле.
+
+import random
+
+def fill_doc(doc):
+    with open(doc, "a", encoding="UTF-8") as f:
+        for _ in range(2500):
+            f.write(str(random.randint(0, 9)))
+
+fill_doc("text.txt")
+
+max = 0
+count = 1
+new_txt = 0
+
+path = "text.txt"
+with open(path, "r", encoding="UTF-8") as f:
+    for i  in f.read():
+        if i == new_txt:
+            count += 1
+        else:
+            count = 1
+        if max < count:
+            max= count
+            txt = i
+        new_txt = i
+
+print(txt * max)
+
+
+
+
